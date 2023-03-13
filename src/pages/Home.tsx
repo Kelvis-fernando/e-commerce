e18c768bb05a4938ca7fc92d100be83bd842d4bd
Kelvis Delapena <kelvisdelapena@gmail.com>
2023-03-13 09:16:01 -0300

    adjusted home

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,22 +1,22 @@
 import Carrossel from "../components/Carrossel";
 import Navbar from "../components/layout/Navbar";
 import { Main } from "../styles/app";
-import { HomeContainer } from "../styles/home";
+import { HomeContainer, HomeProducts } from "../styles/home";
 
 const Home = () => {
   return (
     <Main>
       <Navbar />
       <HomeContainer>
-        <Carrossel
-          images={[
-            "https://cdn.shopify.com/s/files/1/1867/9411/products/KitMaster_Acrilico_1512x.png?v=1664995596",
-            "https://cdn.shopify.com/s/files/1/0421/4764/3548/products/Classico_FazendasMoido.jpg?v=1641680479&width=1100",
-            "https://cdn.shopify.com/s/files/1/1867/9411/products/KitcompletoTM_1200px_1512x.png?v=1654696542",
-          ]}
+        <img
+          src="https://cdn.shopify.com/s/files/1/1867/9411/products/KitMaster_Acrilico_1512x.png?v=1664995596"
+          alt="capa"
         />
-        <h1>items</h1>
-        <h1>bottom</h1>
+        <h1>ESCOLHA O MELHOR CAFÉ PARA VOCÊ</h1>
+        <h2>DO SEU PRODUTOR RURAL FAVORITO</h2>
+        <HomeProducts>
+            <h1>Vindo direto do campo</h1>
+        </HomeProducts>
       </HomeContainer>
     </Main>
   );
--- src/styles/home.ts
+++ src/styles/home.ts
@@ -5,4 +5,34 @@ export const HomeContainer = styled.div`
   flex-direction: column;
   align-items: center;
   padding-top: 0.2rem;
+
+  img {
+    height: 40rem;
+    width: 65rem;
+  }
+
+  h1 {
+    font-size: 3rem;
+    color: #b2bebf;
+    font-weight: 400;
+  }
+  h2 {
+    font-size: 2.5rem;
+    color: #b2bebf;
+    font-weight: 300;
+  }
+`;
+
+export const HomeProducts = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  background-color: #889c9b;
+  width: 98vw;
+  margin-top: 1rem;
+
+  h1 {
+    color: #FFF;
+    font-weight: 400;
+  }
 `;
