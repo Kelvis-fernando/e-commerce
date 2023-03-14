7b98973e2c2f459efe15894929cd7049938c600c
Kelvis Delapena <kelvisdelapena@gmail.com>
2023-03-14 15:17:42 -0300

    fix: added static image to home, and created assets folder

--- src/hooks/products/useProducts.tsx
+++ src/hooks/products/useProducts.tsx
@@ -27,35 +27,7 @@ const useProducts = () => {
       title: "Kit capsulas + Cofee",
       brand: "Baggio",
       image:
-        "https://cdn.shopify.com/s/files/1/1867/9411/products/KitMaster_Acrilico_1512x.png?v=1664995596",
-      price: "249.99",
-      description:
-        "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Illum a aperiam quos non doloribus ",
-    },
-    {
-      id: "hash1",
-      title: "Coffee",
-      brand: "Coffee ++",
-      image:
-        "https://cdn.shopify.com/s/files/1/0421/4764/3548/products/Classico_FazendasMoido.jpg?v=1641680479&width=1100",
-      price: "90.00",
-      description:
-        "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Illum a aperiam quos non doloribus ",
-    },
-    {
-      id: "hash2",
-      title: "Coffee",
-      brand: "Baggio",
-      image:
-        "https://cdn.shopify.com/s/files/1/1867/9411/products/KitcompletoTM_1200px_1512x.png?v=1654696542",
-      price: "120.00",
-    },
-    {
-      id: "hash3",
-      title: "Kit capsulas + Cofee",
-      brand: "Baggio",
-      image:
-        "https://cdn.shopify.com/s/files/1/1867/9411/products/KitMaster_Acrilico_1512x.png?v=1664995596",
+        "https://cdn.shopify.com/s/files/1/1867/9411/products/KitMaster_Acrilico_Truf_1512x.png?v=1678811237",
       price: "249.99",
       description:
         "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Illum a aperiam quos non doloribus ",
@@ -85,7 +57,7 @@ const useProducts = () => {
       title: "Kit capsulas + Cofee",
       brand: "Baggio",
       image:
-        "https://cdn.shopify.com/s/files/1/1867/9411/products/KitMaster_Acrilico_1512x.png?v=1664995596",
+        "https://cdn.shopify.com/s/files/1/1867/9411/products/KitMaster_Acrilico_Truf_1512x.png?v=1678811237",
       price: "249.99",
       description:
         "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Illum a aperiam quos non doloribus ",
@@ -97,6 +69,8 @@ const useProducts = () => {
       image:
         "https://cdn.shopify.com/s/files/1/0421/4764/3548/products/Classico_FazendasMoido.jpg?v=1641680479&width=1100",
       price: "90.00",
+      description:
+        "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Illum a aperiam quos non doloribus ",
     },
     {
       id: "hash2",
@@ -105,14 +79,78 @@ const useProducts = () => {
       image:
         "https://cdn.shopify.com/s/files/1/1867/9411/products/KitcompletoTM_1200px_1512x.png?v=1654696542",
       price: "120.00",
+      description:
+        "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Illum a aperiam quos non doloribus ",
     },
     {
       id: "hash3",
       title: "Kit capsulas + Cofee",
       brand: "Baggio",
       image:
-        "https://cdn.shopify.com/s/files/1/1867/9411/products/KitMaster_Acrilico_1512x.png?v=1664995596",
+        "https://cdn.shopify.com/s/files/1/1867/9411/products/KitMaster_Acrilico_Truf_1512x.png?v=1678811237",
       price: "249.99",
+      description:
+        "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Illum a aperiam quos non doloribus ",
+    },
+    {
+      id: "hash1",
+      title: "Coffee",
+      brand: "Coffee ++",
+      image:
+        "https://cdn.shopify.com/s/files/1/0421/4764/3548/products/Classico_FazendasMoido.jpg?v=1641680479&width=1100",
+      price: "90.00",
+      description:
+        "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Illum a aperiam quos non doloribus ",
+    },
+    {
+      id: "hash2",
+      title: "Coffee",
+      brand: "Baggio",
+      image:
+        "https://cdn.shopify.com/s/files/1/1867/9411/products/KitcompletoTM_1200px_1512x.png?v=1654696542",
+      price: "120.00",
+      description:
+        "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Illum a aperiam quos non doloribus ",
+    },
+    {
+      id: "hash3",
+      title: "Kit capsulas + Cofee",
+      brand: "Baggio",
+      image:
+        "https://cdn.shopify.com/s/files/1/1867/9411/products/KitMaster_Acrilico_Truf_1512x.png?v=1678811237",
+      price: "249.99",
+      description:
+        "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Illum a aperiam quos non doloribus ",
+    },
+    {
+      id: "hash1",
+      title: "Coffee",
+      brand: "Coffee ++",
+      image:
+        "https://cdn.shopify.com/s/files/1/0421/4764/3548/products/Classico_FazendasMoido.jpg?v=1641680479&width=1100",
+      price: "90.00",
+      description:
+        "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Illum a aperiam quos non doloribus ",
+    },
+    {
+      id: "hash2",
+      title: "Coffee",
+      brand: "Baggio",
+      image:
+        "https://cdn.shopify.com/s/files/1/1867/9411/products/KitcompletoTM_1200px_1512x.png?v=1654696542",
+      price: "120.00",
+      description:
+        "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Illum a aperiam quos non doloribus ",
+    },
+    {
+      id: "hash3",
+      title: "Kit capsulas + Cofee",
+      brand: "Baggio",
+      image:
+        "https://cdn.shopify.com/s/files/1/1867/9411/products/KitMaster_Acrilico_Truf_1512x.png?v=1678811237",
+      price: "249.99",
+      description:
+        "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Illum a aperiam quos non doloribus ",
     },
   ];
 
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -2,6 +2,7 @@ import { Link } from "react-router-dom";
 import Navbar from "../components/layout/Navbar";
 import { Main } from "../styles/app";
 import { HomeContainer, HomeWelcome } from "../styles/home";
+import image from "../assets/capa.webp";
 
 const Home = () => {
   return (
@@ -9,15 +10,16 @@ const Home = () => {
       <Navbar />
       <HomeContainer>
         <HomeWelcome>
-            <h1>Vindo direto do campo</h1>
-            <h1>E do seu produtor favorito</h1>
-            <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Neque repellendus vitae rerum suscipit excepturi ex nesciunt illo velit, ipsum.</p>
-            <Link to="/products">Veja os nossos produtos!</Link>
+          <h1>Vindo direto do campo</h1>
+          <h1>E do seu produtor favorito</h1>
+          <p>
+            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Neque
+            repellendus vitae rerum suscipit excepturi ex nesciunt illo velit,
+            ipsum.
+          </p>
+          <Link to="/products">Veja os nossos produtos!</Link>
         </HomeWelcome>
-        <img
-          src="https://cdn.shopify.com/s/files/1/1867/9411/products/KitMaster_Acrilico_1512x.png?v=1664995596"
-          alt="capa"
-        />
+        <img src={image} alt="capa" />
       </HomeContainer>
     </Main>
   );
