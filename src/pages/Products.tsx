afdc2f519d0aa0d65c6fc1a3ffbad3367d4ac27b
Kelvis Delapena <kelvisdelapena@gmail.com>
2023-04-17 08:53:23 -0300

    fix(Filterbar): remove the comp and added to page

--- src/pages/Products.tsx
+++ src/pages/Products.tsx
@@ -1,9 +1,9 @@
-import FilterBar from "../components/layout/FilterBar";
 import Loading from "../components/layout/Loading";
 import Navbar from "../components/layout/Navbar";
 import ProductCard from "../components/products/ProductCard";
 import { UseProducts } from "../hooks/products/useProducts";
 import { Main } from "../styles/app";
+import { FilterBarContainer } from "../styles/layout/filterBar";
 import { ProductsContainer } from "../styles/products/products";
 import { ProductCardProps } from "../types/productCardProps";
 
@@ -13,7 +13,19 @@ const Products = () => {
   return (
     <Main>
       <Navbar />
-      <FilterBar />
+      <FilterBarContainer>
+        <label htmlFor="">Ordernar por:</label>
+        <select name="ordernar" id="">
+          <option value="">Selecione um filtro</option>
+          <option value="">Em destaque</option>
+          <option value="">Mais Vendidos</option>
+          <option value="">Ordem Alfabetica</option>
+          <option value="">Maior Preço</option>
+          <option value="">Menor Preço</option>
+          <option value="">Data Adicionada</option>
+        </select>
+        <input type="text" placeholder="Procure um produto" />
+      </FilterBarContainer>
       <ProductsContainer>
         {products !== undefined ? (
           products?.map((product: ProductCardProps) => (
