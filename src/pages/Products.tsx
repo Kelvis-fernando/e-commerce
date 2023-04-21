9016090695ab537e8f187d2fd9e993f1d2925276
Kelvis Delapena <kelvisdelapena@gmail.com>
2023-04-21 14:27:18 -0300

    fix(products): remove filter by types

--- src/pages/Products.tsx
+++ src/pages/Products.tsx
@@ -1,4 +1,3 @@
-import { useState } from "react";
 import Loading from "../components/layout/Loading";
 import Navbar from "../components/layout/Navbar";
 import ProductCard from "../components/products/ProductCard";
@@ -15,16 +14,6 @@ const Products = () => {
     <Main>
       <Navbar />
       <FilterBarContainer>
-        <label htmlFor="">Ordernar por:</label>
-        <select name="ordernar" id="">
-          <option value="">Selecione um filtro</option>
-          <option value="">Em destaque</option>
-          <option value="">Mais Vendidos</option>
-          <option value="">Ordem Alfabetica</option>
-          <option value="">Maior Preço</option>
-          <option value="">Menor Preço</option>
-          <option value="">Data Adicionada</option>
-        </select>
         <input
           type="text"
           placeholder="Procure um produto"
